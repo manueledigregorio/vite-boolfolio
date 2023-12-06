e45f96d480d0fa3c7577af47f0a06269165684a8
manueledigregorio <dig.manuele@gmail.com>
2023-12-06 20:58:08 +0100

    [ADD] fix card

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -14,13 +14,13 @@ data() {
 <template>
   <div class="container">
     <div class="row">
+      <h1>Card: </h1>
 
      <div v-for="project in store.project" :key="project.id" class="col-4 py-3 ">
       <div class="card h-100 " >
         <div class="card-body">
           <h5 class="card-title"> Name: {{ project.name }}</h5>
           <h6 class="card-subtitle mb-2 text-body-secondary">Description: {{ project.description }}</h6>
-          <p class="card-text">date creation:</p>
         </div>
       </div>
      </div>
